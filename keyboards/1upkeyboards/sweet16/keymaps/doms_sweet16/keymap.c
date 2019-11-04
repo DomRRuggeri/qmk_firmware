30cf17bd1976cffc58a24165470eab724bb690f6
Dominic Ruggeri <druggeri@ellsworth.com>
2019-11-04 17:11:41 -0600

    Added separated RGB lighting

--- keyboards/1upkeyboards/sweet16/keymaps/doms_sweet16/keymap.c
+++ keyboards/1upkeyboards/sweet16/keymaps/doms_sweet16/keymap.c
@@ -35,6 +35,48 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
+uint32_t layer_state_set_user(uint32_t state) {
+  switch (biton32(state)) {
+    case 0:
+      rgblight_mode(RGB_0[0]);
+      rgblight_sethsv(RGB_0[1],RGB_0[2],RGB_0[3]);
+      break;
+
+    case 1:
+      rgblight_mode(RGB_1[0]);
+      rgblight_sethsv(RGB_1[1],RGB_1[2],RGB_1[3]);
+      break;
+
+    case 2:
+      rgblight_mode(RGB_2[0]);
+      rgblight_sethsv(RGB_2[1],RGB_2[2],RGB_2[3]);
+      break;
+
+    case 3:
+      rgblight_mode(RGB_3[0]);
+      rgblight_sethsv(RGB_3[1],RGB_3[2],RGB_3[3]);
+      break;
+
+    case 4:
+      rgblight_mode(RGB_4[0]);
+      rgblight_sethsv(RGB_4[1],RGB_4[2],RGB_4[3]);
+      break;
+  }
+  return state;
+}
+
+extern rgblight_config_t rgblight_config;
+
+void led_set_user(uint8_t usb_led) {
+  if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
+    rgblight_mode(RGB_Caps[0]);
+    rgblight_sethsv(RGB_Caps[1],RGB_Caps[2],RGB_Caps[3]);
+  } else {
+    (layer_state_set_user(layer_state));
+  };
+
+}
+
 void matrix_scan_user(void) {
   if (is_alt_tab_active) {
     if (timer_elapsed(alt_tab_timer) > 1000) {
@@ -43,6 +85,13 @@ void matrix_scan_user(void) {
     }
   }
 }
+
+
+ void shutdown_user()
+    {
+      rgblight_mode(RGB_Reset[0]);
+      rgblight_sethsv(RGB_Reset[1],RGB_Reset[2],RGB_Reset[3]);
+    }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -56,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT_ortho_4x4(
     TO(5),   TO(6),     TO(7),   RESET,
     TO(4),   KC_NO,     KC_NO,   KC_NO, 
-    TO(3),   KC_NO,     KC_NO,   KC_NO, 
+    TO(3),   KC_NO,     KC_NO,   RGB_TOG, 
     TO(2),   KC_NLCK,   ONE,     KC_TRNS
     ),
 
