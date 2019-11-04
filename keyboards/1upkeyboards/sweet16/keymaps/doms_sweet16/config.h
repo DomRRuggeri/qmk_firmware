8896ca5d577246135d8afa813031c9cb7c476a30
Dominic Ruggeri <druggeri@ellsworth.com>
2019-11-04 17:11:15 -0600

    Changes RGB number

--- keyboards/1upkeyboards/sweet16/keymaps/doms_sweet16/config.h
+++ keyboards/1upkeyboards/sweet16/keymaps/doms_sweet16/config.h
@@ -1,9 +1,8 @@
 /* Underglow options */
-//#undef RGB_DI_PIN
-//#define RGB_DI_PIN D7
+
 #define RGBLIGHT_ANIMATIONS
 #undef RGBLED_NUM
-#define RGBLED_NUM 15
+#define RGBLED_NUM 16
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
