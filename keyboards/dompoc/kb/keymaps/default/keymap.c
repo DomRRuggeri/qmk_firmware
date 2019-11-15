d2e8b103d87816d00ed0d85ebf9d5027389d4fba
Dominic Ruggeri <druggeri@ellsworth.com>
2019-11-14 19:54:39 -0600

    Added macros

--- keyboards/dompoc/kb/keymaps/default/keymap.c
+++ keyboards/dompoc/kb/keymaps/default/keymap.c
@@ -1,10 +1,31 @@
 #include "kb.h"
 
+enum custom_keycodes {
+  REBOOT = SAFE_RANGE,
+};
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case REBOOT:
+      if (record->event.pressed) {
+		  SEND_STRING(SS_TAP(X_LGUI));
+		  _delay_ms(300);
+		  SEND_STRING("powershell");
+		  _delay_ms(500);
+		  SEND_STRING(SS_TAP(X_ENTER));
+		  _delay_ms(500);
+		  SEND_STRING("restart-computer -force");
+		  SEND_STRING(SS_TAP(X_ENTER));
+	  }
+  }
+  return true;
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[0] = KEYMAP(
-		KC_2,        LGUI(KC_L),
+		REBOOT,        LGUI(KC_L),
 		LT(1, KC_4), KC_3),
 
 	[1] = KEYMAP(
@@ -74,10 +95,6 @@ void matrix_init_user(void) {
 }
 
 void matrix_scan_user(void) {
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	return true;
 }
 
 void led_set_user(uint8_t usb_led) {
