56153819e26959d1b03b7b1a543c007039941b83
unknown <domrruggeri@gmail.com>
2019-12-05 09:02:27 -0600

    Fixed rotary encoder I think

--- keyboards/pocbox/config.h
+++ keyboards/pocbox/config.h
@@ -44,8 +44,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROW_PINS { F6, B1 }
 #define MATRIX_COL_PINS { D4, D7 }
 
-#define ENCODERS_PAD_A { A8 }
-#define ENCODERS_PAD_B { A9 }
+#define UNUSED_PINS
+
+#define ENCODERS_PAD_A { B4 }
+#define ENCODERS_PAD_B { B5 }
+
 #define ENCODER_RESOLUTION 4
 
 #define DIODE_DIRECTION COL2ROW
