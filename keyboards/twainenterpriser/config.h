a8e4a001705ce5d5b0e17185e5faae78a857752e
Dominic Ruggeri <druggeri@ellsworth.com>
2020-01-02 17:11:01 -0600

    Created twain enterpriser

---
+++ keyboards/twainenterpriser/config.h
@@ -0,0 +1,40 @@
+/* Copyright 2019 Sebastian Williams
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0xB195
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    Dom Ruggeri
+#define PRODUCT         Twain Enterpriser
+#define DESCRIPTION     60% Bluetooth Keyboard
+
+/* key matrix size */
+#define MATRIX_ROWS 1
+#define MATRIX_COLS 1
+
+#define DIODE_DIRECTION COL2ROW
+
+/* Pancake default pinout */
+#define MATRIX_ROW_PINS { F7 }
+#define MATRIX_COL_PINS { F6 }
+#define UNUSED_PINS
+
+#define AdafruitBleResetPin D4
+#define AdafruitBleCSPin B4
+#define AdafruitBleIRQPin E6
---
+++ keyboards/twainenterpriser/readme.md
@@ -0,0 +1,12 @@
+# Twain Enterpriser
+
+Bluetooth 60% Keyboard with an adafruit feather
+
+Keyboard Maintainer: [DomRRuggeri](https://github.com/DomRRuggeri)
+Inspiration: [SparkTwain](http://sparktwain.com)
+
+Make example for this keyboard (after setting up your build environment):
+
+    make twainenterpriser:default
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
