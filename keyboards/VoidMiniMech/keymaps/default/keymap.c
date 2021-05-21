fe0e627b4e471319c81805424f8bb1f46fa0fbc9
DomRRuggeri <40496679+DomRRuggeri@users.noreply.github.com>
2021-05-21 16:36:34 -0500

    Update keymap.c

--- keyboards/VoidMiniMech/keymaps/default/keymap.c
+++ keyboards/VoidMiniMech/keymaps/default/keymap.c
@@ -2,15 +2,29 @@
 
 #define _BASE 0
 
+enum custom_keycodes {
+  UP_URL = SAFE_RANGE
+};
 
 #ifdef OLED_DRIVER_ENABLE
 void oled_task_user(void) {
   // Host Keyboard Layer Status
-  oled_write_P(PSTR("Hello Zack"), false);
-  oled_write_P(PSTR("Line 2"), false);
-  oled_write_P(PSTR("Line 3"), false);
+  oled_write_P(PSTR("Hello Zack.\nI made it work in QMK\n...faster than you! >:)"), false);
 }
 #endif
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case UP_URL:
+            if (record->event.pressed) {
+                SEND_STRING("http://1upkeyboards.com");
+            }
+            return false;
+            break;
+    }
+    return true;
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_default(
